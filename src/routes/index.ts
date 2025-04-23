9103f4580586be0c00f338ea8381e63dffe08f4f
Deedee <deedeeowoeye@gmail.com>
2025-04-23 21:23:58 +0100

    feat: referral service

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import productRoute from './productRoute';
 import orderRoute from './orderRoute';
 import reviewRoute from './reviewRoute';
 import rewardRoute from './rewardRoute';
+import referralRoute from './referralRoute';
 import watchlistRoute from './watchlistRoute';
 
 const router = express.Router();
@@ -15,6 +16,7 @@ router.use('/products', productRoute);
 router.use('/orders', orderRoute);
 router.use('/reviews', reviewRoute);
 router.use('/rewards', rewardRoute);
+router.use('/referral', referralRoute);
 router.use('/watchlist', watchlistRoute);
 
 export default router;
