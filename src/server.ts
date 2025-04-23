8d27d24ec2d66daaa19c9145f8f75673c538c186
Deedee <deedeeowoeye@gmail.com>
2025-04-23 21:39:18 +0100

    chore: websocket integration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
-import app from "./configs/app";
-import config from "./configs/config";
+import app from './configs/app';
+import config from './configs/config';
+import { createServer } from 'http';
+import { WebSocketService } from './services/webSocketService';
+import { NotificationService } from './services/notificationService';
 
 const PORT = config.PORT;
 
-app.listen(PORT, () => {
+const server = createServer(app);
+const webSocketService = new WebSocketService(server);
+NotificationService.initialize(webSocketService);
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
